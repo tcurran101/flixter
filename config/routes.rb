f7ca5ea8bb6b7a00424600cac89aa129ca47a71f
Tiffany Curran <tcurran101@gmail.com>
2019-11-10 16:17:36 +0000

    created enrollments model, control & view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
-  resources :courses, only: [:index, :show]
+  
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create
+  end
+
   resources :lessons, only: [:show]
+  
   namespace :instructor do
     resources :sections, only: [] do
     resources :lessons, only: [:new, :create]
@@ -11,4 +16,5 @@ Rails.application.routes.draw do
       resources :sections, only: [:new, :create]
     end
   end
+
 end
