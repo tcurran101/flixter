f6edeb7feaa9001d29ecf4cbcaf8ceea8e67b47a
Tiffany Curran <tcurran101@gmail.com>
2019-11-24 14:54:57 +0000

    added sortable to sections

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,15 @@ Rails.application.routes.draw do
   end
 
   resources :lessons, only: [:show]
-  namespace :instructor do
+
+    namespace :instructor do
     resources :lessons, only: [:update]
+    resources :sections, only: [:update]
     resources :sections, only: [] do
+      
       resources :lessons, only: [:new, :create]
       end
+      
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
     end
