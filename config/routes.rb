71d9d7dca96809eaa994130c9538333929217fe2
Tiffany Curran <tcurran101@gmail.com>
2019-11-25 13:07:55 +0000

    added new section modal & rm new section page

--- config/routes.rb
+++ config/routes.rb
@@ -8,17 +8,15 @@ Rails.application.routes.draw do
   end
 
   resources :lessons, only: [:show]
-
-    namespace :instructor do
+  namespace :instructor do
     resources :lessons, only: [:update]
-    resources :sections, only: [:update]
-    resources :sections, only: [] do
-      
+    resources :sections, only: [:update] do
       resources :lessons, only: [:new, :create]
-      end
+    end
+      
       
     resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create]
+      resources :sections, only: [:create]
     end
   end
 
