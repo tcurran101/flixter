97ed863462cb4723f6acb591f20dc8e14b3a53ce
Tiffany Curran <tcurran101@gmail.com>
2019-12-07 01:00:52 +0000

    fixed code errors

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -12,18 +12,18 @@ class Instructor::LessonsController < ApplicationController
     current_lesson.update_attributes(lesson_params)
     render plain: 'updated!'
   end
-    
-      
+
+
   private
 
- 
+
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
 
   def require_authorized_for_current_section
     if current_section.course.user != current_user
-      return render plain: 'Unauthorized', status: :unauthorized
+      render plain: 'Unauthorized', status: :unauthorized
     end
   end
 
@@ -38,7 +38,7 @@ class Instructor::LessonsController < ApplicationController
   def current_section
     @current_section ||= Section.find(params[:section_id])
   end
-    
+
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
   end
