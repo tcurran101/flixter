c0392f28ff06efe53265cdfac7b67fa4d1dc4525
Tiffany Curran <tcurran101@gmail.com>
2019-12-09 01:43:08 +0000

    fixed instructor add lesson

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -13,9 +13,13 @@ class Instructor::LessonsController < ApplicationController
     render plain: 'updated!'
   end
 
-
   private
 
+  def require_authorized_for_current_lesson
+    if current_lesson.section.course.user != current_user
+      render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
 
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
@@ -27,13 +31,6 @@ class Instructor::LessonsController < ApplicationController
     end
   end
 
-  def require_authorized_for_current_lesson
-    if current_lesson.section.course.user != current_user
-      render plain: 'Unauthorized', status: :unauthorized
-    end
-  end
-
-
   helper_method :current_section
   def current_section
     @current_section ||= Section.find(params[:section_id])
@@ -42,5 +39,4 @@ class Instructor::LessonsController < ApplicationController
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,18 +6,14 @@ Rails.application.routes.draw do
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
-
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :lessons, only: [:update]
     resources :sections, only: [:update] do
       resources :lessons, only: [:create]
     end
-      
-      
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create]
     end
   end
-
 end
